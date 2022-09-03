8c77001e0248eabd04cfa36373978adbc6c29adb
oyuchan <oyc0401@gmail.com>
2022-09-03 16:33:17 +0900

    init

---
+++ main.py
@@ -0,0 +1,50 @@
+def solution(id_list, report, k):
+    answer = []
+
+    dic = {}
+    count = {}
+    bad_list = list()
+    messageMap = {}
+
+    # 배열 초기화
+    for id in id_list:
+        dic[id] = set()
+        count[id] = 0
+        messageMap[id] = 0
+
+    # 신고한 사람 정리: dic 만들기
+    for name in report:
+        good_id, bad_id = name.split(" ")
+        dic[good_id].add(bad_id)
+
+    # 신고 받은 횟수 정리: count 만들기
+    for key, value in dic.items():
+        for ele in value:
+            print(key, ele)
+            count[ele] += 1
+
+    # 정지당하는 사람 설정: bad_id 만들기
+    for key, val in count.items():
+        print(key, val)
+        if val >= k:
+            bad_list.append(key)
+
+    # 메일 받을 횟수 구하기
+    for key, value in dic.items():
+        for ele in value:
+            for bad in bad_list:
+                print(bad, ele)
+                if bad == ele:
+                    messageMap[key] += 1
+
+    # 리스트 만들기
+    for id in id_list:
+        answer.append(messageMap[id])
+
+    return answer
+
+
+print(
+    solution(["muzi", "frodo", "apeach", "neo"],
+             ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi", "apeach muzi"],
+             2))
